61ce2bc660d8813437683ef5658a0210b70ff1f8
Steve Robbins <steven.j.robbins@gmail.com>
2014-10-28 16:04:56 -0700

    resolves #15, sort direction indication, making rating column sortable

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
     MageHero_App.bindUpvote();
     $('table.listing').tablesorter({
         headers: {
-            0: { sorter: false },
             1: { sorter: false },
             2: { sorter: false },
             4: { sorter: false },
-            7: { sorter: false },
             8: { sorter: false }
+        },
+        textExtraction: function(cell) {
+            var votes = $(cell).find('.vote-count');
+            if (votes.length) {
+                return votes.text();
+            }
+            return $(cell).text();
         }
     });
 });
