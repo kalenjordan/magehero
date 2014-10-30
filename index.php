b5e5bba506e710dbabe4f29cb9709ccc36ffbf19
Cyrill Schumacher <cyrill@schumacher.fm>
2014-10-31 10:13:45 +1100

    Initial commit for showing the users on the world map

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 require_once(dirname(__FILE__) . '/vendor/autoload.php');
 session_start();
 
-spl_autoload_register(function($class) {
-    $parts = explode("_", $class);
+spl_autoload_register(function ($class) {
+    $parts               = explode("_", $class);
     $classSlashSeparated = implode('/', $parts);
-    $pathToFile = "code/$classSlashSeparated.php";
-    $fullPathToFile = dirname(__FILE__) . '/' . $pathToFile;
+    $pathToFile          = "code/$classSlashSeparated.php";
+    $fullPathToFile      = dirname(__FILE__) . '/' . $pathToFile;
 
     if (file_exists($fullPathToFile)) {
         include $fullPathToFile;
@@ -15,11 +15,13 @@ spl_autoload_register(function($class) {
 });
 
 Toro::serve(array(
-     "/"                        => "Controller_Index",
-     "/available"               => "Controller_Available",
-     "/login"                   => "Controller_Login",
-     "/logout"                  => "Controller_Logout",
-     "/profile"                 => "Controller_Profile",
-     "/user/:number/upvote"     => "Controller_UserUpvote",
-     "/(.*)"                    => "Controller_UserProfile",
+    "/"                    => "Controller_Index",
+    "/available"           => "Controller_Available",
+    "/login"               => "Controller_Login",
+    "/logout"              => "Controller_Logout",
+    "/profile"             => "Controller_Profile",
+    "/user/:number/upvote" => "Controller_UserUpvote",
+    "/(.*)"                => "Controller_UserProfile",
+    "/map"                 => "Controller_Map",
+    "/map/users"           => "Controller_MapUsers",
 ));
