4119c9a00e3a769ab5c31ea7a1c6fe6f2e0d5828
Cyrill Schumacher <cyrill@schumacher.fm>
2014-10-31 10:13:45 +1100

    Initial commit for showing the users on the world map

---
+++ code/Controller/MapUsers.php
@@ -0,0 +1,31 @@
+<?php
+
+class Controller_MapUsers extends Controller_Abstract
+{
+    public function get()
+    {
+
+//        example
+//$db = new PDO('sqlite:leaflet.sqlite');
+//$sql = "SELECT id, name, website, city, lat, lng FROM users;";
+//
+//$rs = $db->query($sql);
+//if (!$rs) {
+//    echo "An SQL error occured.\n";
+//    exit;
+//}
+//
+//$rows = array();
+//while($r = $rs->fetch(PDO::FETCH_ASSOC)) {
+//    $rows[] = $r;
+//}
+//print json_encode($rows);
+
+
+	$data = [
+	    'u1' => 'u2'
+	];
+
+	echo json_encode($data);
+    }
+}
--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 require_once(dirname(__FILE__) . '/vendor/autoload.php');
 session_start();
 
-spl_autoload_register(function($class) {
-    $parts = explode("_", $class);
+spl_autoload_register(function ($class) {
+    $parts               = explode("_", $class);
     $classSlashSeparated = implode('/', $parts);
-    $pathToFile = "code/$classSlashSeparated.php";
-    $fullPathToFile = dirname(__FILE__) . '/' . $pathToFile;
+    $pathToFile          = "code/$classSlashSeparated.php";
+    $fullPathToFile      = dirname(__FILE__) . '/' . $pathToFile;
 
     if (file_exists($fullPathToFile)) {
         include $fullPathToFile;
@@ -25,4 +25,13 @@ Toro::serve(array(
     "/user/:number/upvote"     => "Controller_UserUpvote",
     "/:string/posts"           => "Controller_UserPosts",
     "/(.*)"                    => "Controller_UserProfile",
+    "/"                    => "Controller_Index",
+    "/available"           => "Controller_Available",
+    "/login"               => "Controller_Login",
+    "/logout"              => "Controller_Logout",
+    "/profile"             => "Controller_Profile",
+    "/user/:number/upvote" => "Controller_UserUpvote",
+    "/(.*)"                => "Controller_UserProfile",
+    "/map"                 => "Controller_Map",
+    "/map/users"           => "Controller_MapUsers",
 ));
