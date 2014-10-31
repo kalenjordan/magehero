fc4cb8f4a4e8804efb5e11a40a86b1fc0d0a59a2
jamescowie <james@jcowie.co.uk>
2014-10-31 18:24:01 +0000

    Add vagrant support to the project

---
+++ Vagrantfile
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+# ^ Syntax hint
+
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.require_version ">= 1.5.0"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  # Box configuration
+  config.vm.box = "Ubuntu-12.04"
+  config.vm.box_url = 'https://cloud-images.ubuntu.com/vagrant/precise/current/precise-server-cloudimg-i386-vagrant-disk1.box'
+
+  # Network configuration
+  config.vm.network :private_network, ip: "10.199.45.152"
+  config.vm.hostname = "magehero.local"
+  config.ssh.forward_agent = true
+
+  # Provision
+  config.vm.provision :shell, :path => "build/vm_build.sh"
+
+  # Shared folders
+  require 'ffi'
+  mount_options = ["dmode=777", "fmode=777"]
+  opts = if FFI::Platform::IS_WINDOWS
+    { :mount_options => mount_options }
+  else
+    { :nfs => mount_options }
+  end
+
+  config.vm.synced_folder(".", "/vagrant", opts)
+
+  # Virtualbox specific configuration
+  config.vm.provider :virtualbox do |vb|
+    #vb.gui = true
+    vb.customize [
+      "modifyvm", :id,
+      "--memory", "1024",
+      "--name", "magehero.local"
+    ]
+  end
+
+end
---
+++ build/vm_build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+#need this to get 'libapache2-mod-fastcgi'
+sudo sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list
+sudo apt-get update
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+sudo apt-get install -y curl apache2 libapache2-mod-fastcgi php5 php5-fpm php5-cli php5-curl php5-gd php5-mcrypt php5-mysql mysql-server
+
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
+sudo sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 1024M/" /etc/php5/fpm/php.ini
+sudo sed -i "s#date\.timezone.*#date\.timezone = \"Europe\/London\"#" /etc/php5/fpm/php.ini
+
+sudo a2enmod rewrite actions fastcgi alias
+
+sudo bash -c "cat >> /etc/apache2/conf.d/servername.conf <<EOF
+ServerName localhost
+EOF"
+
+WEBROOT="/vagrant/"
+sudo echo "<VirtualHost *:80>
+  DocumentRoot $WEBROOT
+
+  <Directory $WEBROOT>
+    Options FollowSymLinks MultiViews ExecCGI
+    AllowOverride All
+    Order deny,allow
+    Allow from all
+  </Directory>
+
+  <IfModule mod_fastcgi.c>
+    AddHandler php5-fcgi .php
+    Action php5-fcgi /php5-fcgi
+    Alias /php5-fcgi /usr/lib/cgi-bin/php5-fcgi
+    FastCgiExternalServer /usr/lib/cgi-bin/php5-fcgi -host 127.0.0.1:9000 -pass-header Authorization
+  </IfModule>
+
+</VirtualHost>" | sudo tee /etc/apache2/sites-available/default > /dev/null
+
+sudo service apache2 restart
+sudo service php5-fpm restart
+
+mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS magehero;"
+mysql -uroot -proot magehero < /vagrant/sql/mysql-1.0.0.sql
+mysql -uroot -proot magehero < /vagrant/sql/mysql-1.0.1.sql
+
+sudo bash -c "cat >> /etc/hosts <<EOF
+127.0.0.1 magehero.local
+EOF"
