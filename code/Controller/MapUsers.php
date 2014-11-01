958ad51a619fc68469f74c1e51e1596e187bc0bc
Cyrill Schumacher <cyrill@schumacher.fm>
2014-11-02 10:27:46 +1100

    Removed short variable and key names

--- code/Controller/MapUsers.php
+++ code/Controller/MapUsers.php
@@ -5,30 +5,30 @@ class Controller_MapUsers extends Controller_Abstract
 
     protected function _getDevelopers()
     {
-        $query      = $this->_getContainer()->User()->selectAll();
-        $userRows   = $this->_getContainer()->LocalConfig()->database()->fetchAll($query);
+        $query = $this->_getContainer()->User()->selectAll();
+        $userRows = $this->_getContainer()->LocalConfig()->database()->fetchAll($query);
         $userModels = array();
         foreach ($userRows as $userRow) {
-            $userModel    = $this->_getContainer()->User()->setData($userRow);
+            $userModel = $this->_getContainer()->User()->setData($userRow);
             $userModels[] = array(
-                'lat'      => $userModel->getLatitude(),
-                'lng'      => $userModel->getLongitude(),
-                'name'     => $userModel->getName(),
-                'company'  => $userModel->getCompany(),
-                'city'     => $userModel->getLocation(),
-                'username' => $userModel->getUsername(),
-                'website'  => $userModel->getWebsiteUrl(),
-                'tw'       => $userModel->getTwitterUsername(),
-                'gh'       => $userModel->getGithubUsername(),
-                'so'       => $userModel->stackoverflowUrl(),
-                'li'       => $userModel->linkedinUrl(),
-                'img'      => $userModel->getImageUrl(),
-                'certs'    => array(
-                    'd' => $userModel->getCertifiedDeveloperUrl(),
-                    'p' => $userModel->certifiedDeveloperPlusUrl(),
-                    'f' => $userModel->certifiedFrontendDeveloperUrl(),
-                    's' => $userModel->certifiedSolutionSpecialistUrl(),
-                    'b' => $userModel->certificationBoardUrl(),
+                'latitude'  => $userModel->getLatitude(),
+                'longitude' => $userModel->getLongitude(),
+                'name'      => $userModel->getName(),
+                'company'   => $userModel->getCompany(),
+                'city'      => $userModel->getLocation(),
+                'username'  => $userModel->getUsername(),
+                'image'     => $userModel->getImageUrl(),
+                'links'     => array(
+                    'website'            => $userModel->getWebsiteUrl(),
+                    'twitter'            => $userModel->getTwitterUsername(),
+                    'github'             => $userModel->getGithubUsername(),
+                    'stackOverflow'      => $userModel->stackoverflowUrl(),
+                    'linkedIn'           => $userModel->linkedinUrl(),
+                    'developer'          => $userModel->getCertifiedDeveloperUrl(),
+                    'developerPlus'      => $userModel->certifiedDeveloperPlusUrl(),
+                    'developerFrontend'  => $userModel->certifiedFrontendDeveloperUrl(),
+                    'solutionSpecialist' => $userModel->certifiedSolutionSpecialistUrl(),
+                    'certificationBoard' => $userModel->certificationBoardUrl(),
                 ),
             );
         }
