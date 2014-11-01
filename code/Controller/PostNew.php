7df6ccfef26a0136201d8543a828306e06e074a5
Kalen Jordan <kalen@sparcksoft.com>
2014-11-01 14:15:16 -0700

    Only allow people with one vote to post

--- code/Controller/PostNew.php
+++ code/Controller/PostNew.php
@@ -4,6 +4,11 @@ class Controller_PostNew extends Controller_Abstract
 {
     public function get()
     {
+        $minimumVoteCount = $this->_getContainer()->LocalConfig()->getRankStarMinimumVotecount();
+        if ($this->_getCurrentUser()->getVoteCount() < $minimumVoteCount) {
+            die("You have to have $minimumVoteCount vote(s) in order to post");
+        }
+
         $data = array(
             "subject"   => "New Post",
             "is_active" => false,
