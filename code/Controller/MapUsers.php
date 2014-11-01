67ab76319e9779f1d185f05ed4ed212549f23629
Cyrill Schumacher <cyrill@schumacher.fm>
2014-11-01 19:51:20 +1100

    Users on map with all URLs

--- code/Controller/MapUsers.php
+++ code/Controller/MapUsers.php
@@ -5,29 +5,38 @@ class Controller_MapUsers extends Controller_Abstract
 
     protected function _getDevelopers()
     {
-	$query      = $this->_getContainer()->User()->selectAll();
-	$userRows   = $this->_getContainer()->LocalConfig()->database()->fetchAll($query);
-	$userModels = array();
-	foreach ($userRows as $userRow) {
-	    $userModel    = $this->_getContainer()->User()->setData($userRow);
-	    $userModels[] = array(
-		'lat'      => $userModel->getLatitude(),
-		'lng'      => $userModel->getLongitude(),
-		'name'     => $userModel->getName(),
-		'company'  => $userModel->getCompany(),
-		'username' => $userModel->getUsername(),
-		'website'  => $userModel->getWebsiteUrl(),
-		'tw'       => $userModel->getTwitterUsername(),
-		'gh'       => $userModel->getGithubUsername(),
-		'img'      => $userModel->getImageUrl(),
-	    );
-	}
-
-	return $userModels;
+        $query      = $this->_getContainer()->User()->selectAll();
+        $userRows   = $this->_getContainer()->LocalConfig()->database()->fetchAll($query);
+        $userModels = array();
+        foreach ($userRows as $userRow) {
+            $userModel    = $this->_getContainer()->User()->setData($userRow);
+            $userModels[] = array(
+                'lat'      => $userModel->getLatitude(),
+                'lng'      => $userModel->getLongitude(),
+                'name'     => $userModel->getName(),
+                'company'  => $userModel->getCompany(),
+                'city'     => $userModel->getLocation(),
+                'username' => $userModel->getUsername(),
+                'website'  => $userModel->getWebsiteUrl(),
+                'tw'       => $userModel->getTwitterUsername(),
+                'gh'       => $userModel->getGithubUsername(),
+                'so'       => $userModel->stackoverflowUrl(),
+                'li'       => $userModel->linkedinUrl(),
+                'img'      => $userModel->getImageUrl(),
+                'certs'    => array(
+                    'd' => $userModel->getCertifiedDeveloperUrl(),
+                    'p' => $userModel->certifiedDeveloperPlusUrl(),
+                    'f' => $userModel->certifiedFrontendDeveloperUrl(),
+                    's' => $userModel->certifiedSolutionSpecialistUrl(),
+                    'b' => $userModel->certificationBoardUrl(),
+                ),
+            );
+        }
+        return $userModels;
     }
 
     public function get()
     {
-	$this->_jsonResponse($this->_getDevelopers());
+        $this->_jsonResponse($this->_getDevelopers());
     }
 }
