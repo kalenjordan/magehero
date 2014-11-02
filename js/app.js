75be691c1ea4c7dfcd8d5d91e7b52e9a9fedd387
Tanel Raja <tanel.raja@multon.ee>
2014-11-02 12:20:23 +0200

    Added list numbers

--- js/app.js
+++ js/app.js
@@ -1,19 +1,43 @@
 $(document).ready(function() {
     MageHero_App.bindUpvote();
+    $.tablesorter.addWidget({
+        id: 'numbers',
+        format: function(table) {
+			var header = table.rows[0].cells[0];
+			var isColumn = (header.innerHTML == '#');
+			if (!isColumn) {
+				//var col = table.rows[0].insertCell(0);
+				var col = table.rows[0].insertBefore(document.createElement('th'), table.rows[0].cells[0]);
+                col.innerHTML = '#';
+                col.className += 'right';
+			}
+			var col;
+			for (var i = 1, row; row = table.rows[i]; i++) {
+				if (isColumn) {
+					table.rows[i].cells[0].innerHTML = i + '.';
+				} else {
+					col = table.rows[i].insertCell(0);
+					col.innerHTML = i + '.';
+                    col.className += ' right';
+				}	
+			}
+        }
+    });
     $('table.listing').tablesorter({
-        headers: {
-            0: { sorter: false },
-            2: { sorter: false },
-            4: { sorter: false },
-            8: { sorter: false }
-        },
+	    headers: {
+		    0: { sorter: false },
+		    2: { sorter: false },
+		    5: { sorter: false },
+		    6: { sorter: false }
+	    },
         textExtraction: function(cell) {
             var votes = $(cell).find('.vote-count');
             if (votes.length) {
                 return votes.text();
             }
             return $(cell).text();
-        }
+        },
+	    widgets: ['numbers']
     });
 });
 
