c1395ab5a4b4459bb5f9f3e20e7e59be3f56a715
Tom Robertshaw <me@tomrobertshaw.net>
2015-01-06 21:39:31 +0000

    Add post search functionality.

--- code/Controller/Search.php
+++ code/Controller/Search.php
@@ -4,7 +4,13 @@ class Controller_Search extends Controller_Abstract
 {
     public function get()
     {
+
+        $posts = $this->_getContainer()->Post()->fetchByTerm($_GET['query']);
+
         echo $this->_getTwig()->render('search.html.twig', array(
+            'session'       => $this->_getSession(),
+            'term'          => htmlspecialchars($_GET['query']),
+            'posts'         => $posts,
             'local_config'  => $this->_getContainer()->LocalConfig(),
         ));
     }
