8b11597a5b3a51aa4fc5600696ea724ac4b10f21
Tom Robertshaw <me@tomrobertshaw.net>
2015-01-07 21:22:28 +0000

    Add tag listing so now we can visit a page that shows all posts for a particular tag.

---
+++ code/Controller/Tag.php
@@ -0,0 +1,25 @@
+<?php
+
+class Controller_Tag extends Controller_Abstract
+{
+    public function get($tagId, $slug = null)
+    {
+
+        $tag = $this->_getContainer()->Tag()->load($tagId);
+
+        // While we only need post ID, ensure that we use post slug for SEO
+        if ($slug != $tag->getSlug()) {
+            header("Location: " . $tag->getUrl(), true, 301);
+        }
+
+        $posts = $this->_getContainer()->Post()->fetchByTagId($tagId);
+
+        echo $this->_getTwig()->render('tag.html.twig', array(
+            'session'       => $this->_getSession(),
+            'tag'           => $tag,
+            'posts'         => $posts,
+            'local_config'  => $this->_getContainer()->LocalConfig(),
+        ));
+    }
+
+}
